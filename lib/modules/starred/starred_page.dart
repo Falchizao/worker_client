690329822bf60520120d5aab94d4a43c861a409a
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-02-25 15:18:51 -0300

    created starred page

---
+++ lib/modules/starred/starred_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StarredPage extends StatefulWidget {
+  @override
+  _StarredPageState createState() => _StarredPageState();
+}
+
+class _StarredPageState extends State<StarredPage> {
+  int currentPage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("StarredPage");
+  }
+}

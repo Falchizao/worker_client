d636d54cea080a4e64e0c1c06b08f7951a60e027
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-02-25 15:16:53 -0300

    standardized btn color

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,2 +1,7 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+
 const String LOGIN = 'authorization/login';
 const String REGISTER = 'authorization/register';
+
+const Color MAINBTNCOLOR = Colors.black;

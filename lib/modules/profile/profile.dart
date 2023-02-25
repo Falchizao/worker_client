84c40cb626eb5fceadd7bf4c530c04949cfb9b6c
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-02-25 16:31:12 -0300

    feat: add profile and bg pic from gallery

--- lib/modules/profile/profile.dart
+++ lib/modules/profile/profile.dart
@@ -1,63 +1,79 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 import 'package:faker/faker.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  File _backgroundImage = File('images/baam.png');
+  File _profileImage = File('images/baam.png');
+  final picker = ImagePicker();
+
+  Future<void> _selectBackgroundImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        _backgroundImage = File(pickedFile.path);
+      }
+    });
+  }
+
+  Future<void> _selectProfileImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        _profileImage = File(pickedFile.path);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: 100,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: true
-                    ? NetworkImage(
-                        "${faker.image.image()}?random=${DateTime.now()}")
-                    : AssetImage('assets/images/noImageAvailable.png')
-                        as ImageProvider,
-                fit: BoxFit.cover,
-              ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 16),
+            GestureDetector(
+              onTap: _selectBackgroundImage,
+              child: _backgroundImage == null
+                  ? Container(
+                      color: Colors.grey[300],
+                      height: 200,
+                      child: const Center(
+                        child: Text('Select the background image'),
+                      ),
+                    )
+                  : Image.file(
+                      _backgroundImage,
+                      height: 200,
+                      fit: BoxFit.cover,
+                    ),
             ),
-          ),
-          const SizedBox(height: 20),
-          Container(
-            height: 100,
-            width: 120,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                image: true
-                    ? NetworkImage(
-                        "${faker.image.image()}?random=${DateTime.now()}")
-                    : AssetImage('assets/images/noImageAvailable.png')
-                        as ImageProvider,
-                fit: BoxFit.cover,
-              ),
+            const SizedBox(height: 10),
+            GestureDetector(
+              onTap: _selectProfileImage,
+              child: _profileImage == null
+                  ? const CircleAvatar(
+                      radius: 50,
+                      child: Icon(Icons.person),
+                    )
+                  : CircleAvatar(
+                      radius: 50,
+                      backgroundImage: FileImage(_profileImage),
+                    ),
             ),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            faker.person.firstName(),
-            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 10),
-          Text(
-            faker.job.title(),
-            style: const TextStyle(fontSize: 18, color: Colors.grey),
-          ),
-          const SizedBox(height: 20),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Text(
-              faker.address.country(),
-              style: const TextStyle(fontSize: 16),
+            const SizedBox(height: 16),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Save'),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

3a3b917b8b7f36e86aa040fb213c558bbd244c72
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-03-18 10:53:09 -0300

    refactor: changing info page layout

--- lib/modules/content/lets_start.dart
+++ lib/modules/content/lets_start.dart
@@ -32,38 +32,36 @@ class _LetsGetStartedPageState extends State<LetsGetStartedPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-              'Create your portfolio and search for jobs',
-              style: GoogleFonts.roboto(
-                fontSize: 35,
-                color: Colors.lightBlue,
-                fontWeight: FontWeight.bold,
-              ),
+        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+          Text(
+            'Create your portfolio and search for jobs',
+            textAlign: TextAlign.center,
+            textDirection: TextDirection.ltr,
+            style: GoogleFonts.roboto(
+              fontSize: 25,
+              color: Colors.lightBlue,
+              fontWeight: FontWeight.bold,
             ),
-            const SizedBox(
-              height: 5.0,
+          ),
+          Text(
+            'easily',
+            textAlign: TextAlign.center,
+            textDirection: TextDirection.ltr,
+            style: GoogleFonts.pacifico(
+              fontSize: 32.0,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
             ),
-            Text(
-              'easily',
-              textAlign: TextAlign.left,
-              style: GoogleFonts.pacifico(
-                fontSize: 32.0,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 200),
-            ElevatedButton(
-              onPressed: () {
-                Get.to(() => SelectPage());
-              },
-              child: const Text('Let`s get started'),
-            ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 200),
+        ]),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Get.to(() => SelectPage());
+        },
+        label: const Text('Next'),
+        backgroundColor: Colors.blue,
       ),
     );
   }

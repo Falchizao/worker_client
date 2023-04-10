a46c5e344fc2f3628854e47e1600c440bd5e8903
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-04-09 23:26:45 -0300

    feat: fetch users profiles services

---
+++ lib/modules/profile/user_data_profile.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
+import '../../utils/handler.dart';
+import '../../utils/requests.dart';
+import '../../services/jwtservice.dart';
+import '../../models/user_model.dart';
+import 'dart:convert';
+
+class UserProfileVisualizer extends StatefulWidget {
+  final User user;
+
+  UserProfileVisualizer({super.key, required this.user});
+
+  @override
+  _UserProfileVisualizerState createState() =>
+      _UserProfileVisualizerState(this.user);
+}
+
+class _UserProfileVisualizerState extends State<UserProfileVisualizer> {
+  _UserProfileVisualizerState(this.user);
+  late String username = user.username;
+  final User user;
+  String userrole = "";
+  bool _isFollowing = false;
+  String userDescription = "";
+  late var token;
+  bool isLoading = true;
+  @override
+  void initState() {
+    super.initState();
+    _fetchUserDetails();
+    Future.delayed(Duration(seconds: 3), () {
+      setState(() {
+        isLoading = false;
+      });
+    });
+  }
+
+  Future<void> _fetchUserDetails() async {
+    token = await JwtService().getToken();
+    var url = '$BASE_URL/user/findByUsername?name=$username';
+
+    final response = await http.post(Uri.parse(url), headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token'
+    });
+
+    if (response.statusCode == 200) {
+      final userDetails = jsonDecode(response.body);
+      userDescription = userDetails["profile"]["description"];
+    } else {
+      handleToast("Error fetching user data");
+    }
+  }
+
+  Future<void> _followUser() async {
+    // Replace the URL with your API endpoint
+    final response = await http.post(
+        Uri.parse('$BASE_URL/connections/follow?name=$username'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token'
+        });
+
+    if (response.statusCode == 200) {
+      setState(() {
+        _isFollowing = !_isFollowing;
+      });
+    } else {
+      // Handle the error accordingly
+      print('Failed to follow user.');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'User profile',
+            style: GoogleFonts.pacifico(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        body: isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : SingleChildScrollView(
+                child: FutureBuilder(
+                    future: _fetchUserDetails(),
+                    builder: (context, snapshot) {
+                      return Column(
+                        children: [
+                          Container(
+                            width: double.infinity,
+                            height: 200,
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                                colors: [
+                                  Colors.deepPurple,
+                                  Colors.deepPurple.shade200
+                                ],
+                              ),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                CircleAvatar(
+                                  radius: 50,
+                                  backgroundImage: NetworkImage(
+                                      'https://api.dicebear.com/6.x/initials/jpg?seed=$username'),
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  username,
+                                  style: const TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
+                                ),
+                                const SizedBox(height: 10),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  'Description',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  userDescription ?? "",
+                                  style: TextStyle(fontSize: 16),
+                                  textAlign: TextAlign.justify,
+                                ),
+                                const SizedBox(height: 20),
+                                Center(
+                                  child: ElevatedButton(
+                                    onPressed: _followUser,
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Colors.deepPurple,
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 40, vertical: 12),
+                                    ),
+                                    child: Text(
+                                        _isFollowing ? 'Unfollow' : 'Follow',
+                                        style: const TextStyle(fontSize: 18)),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      );
+                    }),
+              ));
+  }
+}
--- lib/modules/search/search_page.dart
+++ lib/modules/search/search_page.dart
@@ -1,6 +1,15 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:google_fonts/google_fonts.dart';
+import "package:http/http.dart" as http;
+import 'package:scarlet_graph/models/user_model.dart';
+import '../../services/jwtservice.dart';
+import '../../utils/handler.dart';
+import '../../utils/requests.dart';
+import '../profile/user_data_profile.dart';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -8,94 +17,198 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
-  final TextEditingController _searchController = TextEditingController();
+  final scrollController = ScrollController();
+  bool isFetchingUsers = false;
+  String query = '';
+  int fetchPage = 0;
+  List<User> users = [];
+  Timer? debounce;
+
+  Future<void> fetchUsers() async {
+    var url = '$BASE_URL/user/page?size=10&page=$fetchPage';
+
+    var token = await JwtService().getToken();
+    final response = await http.get(Uri.parse(url), headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token'
+    });
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      List<User> usersList = [];
+      for (var userData in json['content']) {
+        final user = User(
+          userData["email"],
+          userData["username"],
+          "",
+          //userData["role"],
+        );
+        usersList.add(user);
+      }
+      setState(
+        () {
+          users = users + usersList;
+        },
+      );
+    } else {
+      handleToast("Error fetching users");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(_scrollListener);
+    fetchUsers();
+  }
+
+  void searchUsers(String query) {
+    if (debounce?.isActive ?? false) {
+      debounce!.cancel();
+    }
+    debounce = Timer(const Duration(milliseconds: 500), () async {
+      var url = '$BASE_URL/user/fetchallByUsername';
+
+      var token = await JwtService().getToken();
+      final response = await http.post(Uri.parse(url),
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer $token'
+          },
+          body: json.encode({
+            'userStr': query,
+          }));
+      if (response.statusCode == 200) {
+        final json = jsonDecode(response.body);
+        List<User> usersList = [];
+        for (var userData in json) {
+          final user = User(
+            userData["email"],
+            userData["username"],
+            "",
+            //userData["role"],
+          );
+          usersList.add(user);
+        }
+        setState(
+          () {
+            users = usersList;
+          },
+        );
+      } else {
+        handleToast("Error fetching users by label");
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade200,
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: TextField(
-          controller: _searchController,
-          decoration: const InputDecoration(
-            hintText: 'Search',
-            border: InputBorder.none,
+        title: Text(
+          'Search for users',
+          style: GoogleFonts.pacifico(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
           ),
         ),
-        actions: const <Widget>[
-          // IconButton(
-          //   icon: SvgPicture.asset('assets/icons/search.svg'),
-          //   onPressed: () {},
-          // ),
-        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: StaggeredGridView.countBuilder(
-          crossAxisCount: 4,
-          itemCount: 10,
-          itemBuilder: (BuildContext context, int index) {
-            return GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DetailScreen(),
-                  ),
-                );
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20.0),
+            const SizedBox(height: 10.0),
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  query = value;
+                });
+
+                if (value == "") {
+                  setState(
+                    () {
+                      users.clear();
+                    },
+                  );
+                  fetchPage = 0;
+                  users.clear;
+                  fetchUsers();
+                } else {
+                  searchUsers(value);
+                }
               },
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.grey[300],
+              decoration: InputDecoration(
+                hintText: 'Enter a username',
+                filled: true,
+                fillColor: Colors.grey[200],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide.none,
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      height: 100,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        // image: DecorationImage(
-                        //   image: AssetImage('assets/images/image_$index.jpg'),
-                        //   fit: BoxFit.cover,
-                        // ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Categoria $index',
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ),
-                  ],
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.search, color: Colors.deepPurple),
+                  onPressed: () {
+                    searchUsers(query);
+                  },
                 ),
               ),
-            );
-          },
-          staggeredTileBuilder: (int index) => const StaggeredTile.fit(2),
-          mainAxisSpacing: 8.0,
-          crossAxisSpacing: 8.0,
+            ),
+            const SizedBox(height: 20.0),
+            Expanded(
+                child: SizedBox(
+                    height: 200.0,
+                    child: ListView.builder(
+                      physics: const BouncingScrollPhysics(),
+                      padding: const EdgeInsets.all(5.0),
+                      controller: scrollController,
+                      itemCount:
+                          isFetchingUsers ? users.length + 1 : users.length,
+                      itemBuilder: (context, index) {
+                        if (index < users.length) {
+                          final user = users[index];
+                          final String username = user.username;
+                          return Card(
+                            child: ListTile(
+                              onTap: () {
+                                Get.to(() => UserProfileVisualizer(
+                                      user: user,
+                                    ));
+                              },
+                              leading: CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                    'https://api.dicebear.com/6.x/initials/jpg?seed=$username'),
+                              ),
+                              title: Text(user.username.toString()),
+                            ),
+                          );
+                        } else {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                      },
+                    )))
+          ],
         ),
       ),
     );
   }
-}
 
-class DetailScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Categoria selecionada'),
-      ),
-      body: const Center(
-        child: Text('Detalhe Categoria'),
-      ),
-    );
+  Future<void> _scrollListener() async {
+    if (isFetchingUsers) return;
+    if (scrollController.position.pixels ==
+        scrollController.position.maxScrollExtent) {
+      setState(() {
+        isFetchingUsers = true;
+      });
+      fetchPage = fetchPage + 1;
+      await fetchUsers();
+      setState(() {
+        isFetchingUsers = false;
+      });
+    }
   }
 }

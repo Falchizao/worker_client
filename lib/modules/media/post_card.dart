9b55d9cb7105d5ae2cb551312742bef4f343779b
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-04-18 22:43:14 -0300

    feat: clickable user post routing to userprofile

--- lib/modules/media/post_card.dart
+++ lib/modules/media/post_card.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../../models/post_model.dart';
+import '../../models/user_model.dart';
+import '../profile/user_data_profile.dart';
 
 class PostCard extends StatelessWidget {
   final Post post;
@@ -10,43 +13,51 @@ class PostCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-      elevation: 4,
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      'https://api.dicebear.com/6.x/initials/jpg?seed=$viewImageUser'),
-                  radius: 24,
-                ),
-                const SizedBox(width: 10),
-                Text(
-                  post.creatorUsername,
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+    return InkWell(
+      onTap: () {
+        User user = User('', post.creatorUsername, '');
+        Get.to(() => UserProfileVisualizer(
+              user: user,
+            ));
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        elevation: 4,
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://api.dicebear.com/6.x/initials/jpg?seed=$viewImageUser'),
+                    radius: 24,
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Text(
-              post.content,
-              style: const TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              'Posted on ${post.createdDate}',
-              style: const TextStyle(color: Colors.grey, fontSize: 12),
-            ),
-          ],
+                  const SizedBox(width: 10),
+                  Text(
+                    post.creatorUsername,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Text(
+                post.content,
+                style: const TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Posted on ${post.createdDate}',
+                style: const TextStyle(color: Colors.grey, fontSize: 12),
+              ),
+            ],
+          ),
         ),
       ),
     );

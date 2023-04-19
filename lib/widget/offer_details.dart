19c5d205b8e8b282ed87c57372f448a07facd9ad
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-04-18 22:46:37 -0300

    added more labels to offer details

--- lib/widget/offer_details.dart
+++ lib/widget/offer_details.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import "package:http/http.dart" as http;
 import '../models/offer_model.dart';
 import '../services/jwtservice.dart';
@@ -10,39 +11,137 @@ import '../utils/requests.dart';
 
 class JobOfferDetails extends StatelessWidget {
   final Offer offer;
+  late String title = offer.title;
 
-  const JobOfferDetails({super.key, required this.offer});
+  JobOfferDetails({super.key, required this.offer});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(offer.title)),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              offer.title,
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            Text(offer.content),
-            const SizedBox(height: 10),
-            Text('Salary: ${offer.salary}'),
-            const SizedBox(height: 20),
-            Text(offer.hours.toString()),
-            const Spacer(),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  applyToOffer();
-                },
-                child: Text('Apply'.tr),
+      appBar: AppBar(
+          title: Text(offer.title,
+              style: GoogleFonts.pacifico(
+                  color: Colors.white, fontWeight: FontWeight.bold))),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: FadeInImage.assetNetwork(
+                    placeholder: 'images/Reload.gif',
+                    image:
+                        'https://api.dicebear.com/6.x/initials/jpg?seed=$title',
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    height: MediaQuery.of(context).size.width * 0.5,
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-            ),
-          ],
+              const SizedBox(height: 16),
+              Text(
+                offer.title,
+                style:
+                    const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                'Created by ${offer.employer}',
+                style: const TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Type',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                offer.hours > 6 ? 'Full-Time' : 'Half-Time',
+                style: const TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Hours per day',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                '${offer.hours} hours',
+                style: const TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Salary',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                '\$${offer.salary}',
+                style: const TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Location',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                'location here',
+                style: const TextStyle(fontSize: 16),
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Job Description',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                offer.content,
+                style: const TextStyle(fontSize: 16),
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Requirements',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              const Text(
+                "requirements here",
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(height: 24),
+              const Text(
+                'Accepts remote?',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const Text(
+                "remote here",
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(height: 24),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    applyToOffer();
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                    child: Text(
+                      'Apply',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

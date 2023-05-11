17d522934bb6bf08b9493678897c83a89f5e3ee7
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-05-10 22:12:30 -0300

    added snackbar widget

---
+++ lib/widget/snackbar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void showSnackbar(context, color, message) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(
+        message,
+        style: const TextStyle(fontSize: 14),
+      ),
+      backgroundColor: color,
+      duration: const Duration(seconds: 2),
+      action: SnackBarAction(
+        label: "OK",
+        onPressed: () {},
+        textColor: Colors.white,
+      ),
+    ),
+  );
+}

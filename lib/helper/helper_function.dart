76848e5f3dbadd16c5e9eb8e65795f0a38d0cd09
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-05-10 22:16:55 -0300

    helper handler

---
+++ lib/helper/helper_function.dart
@@ -0,0 +1,49 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HelperFunctions {
+  //keys
+  static String userLoggedInKey = "LOGGEDINKEY";
+  static String userNameKey = "USERNAMEKEY";
+  static String userEmailKey = "USEREMAILKEY";
+  static String userPasswordKey = "USERPASSWORDKEY";
+
+  static Future<bool> saveUserLoggedInStatus(bool isUserLoggedIn) async {
+    SharedPreferences sf = await SharedPreferences.getInstance();
+    return await sf.setBool(userLoggedInKey, isUserLoggedIn);
+  }
+
+  static Future<bool> saveUserNameSF(String userName) async {
+    SharedPreferences sf = await SharedPreferences.getInstance();
+    return await sf.setString(userNameKey, userName);
+  }
+
+  static Future<bool> saveUserEmailSF(String userEmail) async {
+    SharedPreferences sf = await SharedPreferences.getInstance();
+    return await sf.setString(userEmailKey, userEmail);
+  }
+
+  static Future<bool> saveUserPasswordSF(String password) async {
+    SharedPreferences sf = await SharedPreferences.getInstance();
+    return await sf.setString(userPasswordKey, password);
+  }
+
+  static Future<String?> getUserPasswordSF() async {
+    SharedPreferences sf = await SharedPreferences.getInstance();
+    return sf.getString(userPasswordKey);
+  }
+
+  static Future<bool?> getUserLoggedInStatus() async {
+    SharedPreferences sf = await SharedPreferences.getInstance();
+    return sf.getBool(userLoggedInKey);
+  }
+
+  static Future<String?> getUserEmailFromSF() async {
+    SharedPreferences sf = await SharedPreferences.getInstance();
+    return sf.getString(userEmailKey);
+  }
+
+  static Future<String?> getUserNameFromSF() async {
+    SharedPreferences sf = await SharedPreferences.getInstance();
+    return sf.getString(userNameKey);
+  }
+}
